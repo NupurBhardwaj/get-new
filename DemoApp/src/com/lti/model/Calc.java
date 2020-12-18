896bfc6ead7194ffa77d399e8f045a6b00e643a8
NupurBhardwaj <76148660+NupurBhardwaj@users.noreply.github.com>
2020-12-18 16:32:39 +0530

    Update Calc.java

--- DemoApp/src/com/lti/model/Calc.java
+++ DemoApp/src/com/lti/model/Calc.java
@@ -2,9 +2,6 @@ package com.lti.model;
 
 public class Calc {
      public void add(int num1,int num2) {
-    	 System.out.println("Sum= "+(num1+num2));
-     }
-     public void subs(int num1,int num2) {
-    	 System.out.println("Diff= "+(num1-num2));
+    	 System.out.println("Sums= "+(num1+num2));
      }
 }
